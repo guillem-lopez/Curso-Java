22d977fac117dff7867f6eba2e7ad748abc5947d
Guillem López <guillem.lopez.fernandez@gmail.com>
2024-06-12 11:50:26 +0200

    Exercise 1 done

--- ForExercises/src/Main.java
+++ ForExercises/src/Main.java
@@ -2,6 +2,12 @@
 public class Main {
     public static void main(String[] args) {
 
+        for (int i = 1; i <= 100; i++) {
+            if (i % 5 == 0) {
+                System.out.println(i);
+            }
+
+        }
 
     }
 }
